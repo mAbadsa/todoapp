af6829c571ed08a146dadd42029f1c08ef8e086c
Muhammad Alabadsa <moh22389@gmail.com>
2020-12-19 10:47:27 +0200

    setup server log for all request Relates #10 By using morgam libary

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const { join } = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const { createStream } = require('rotating-file-stream');
+const morgan = require('morgan');
 
 require('dotenv').config({ path: './config.env' });
 
@@ -11,6 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(join(__dirname, '..', 'public')));
 app.use(cookieParser());
+
+// log all requests in the Apache combined format to the file access.log.
+// create a write stream (in append mode)
+const accessLogStream = createStream('access.log', {
+  interval: '1d',
+  size: '2M',
+  path: join(__dirname, 'log'),
+});
+// setup looger
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // test route
 app.get('/', (req, res) => {
