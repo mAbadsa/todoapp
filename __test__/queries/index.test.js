d6975141e19c703ad923610c93b4a0cf8afa63d5
Muhammad Alabadsa <moh22389@gmail.com>
2020-12-19 09:15:14 +0200

    setup eslint, install all needed package Relates #5

---
+++ __test__/queries/index.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Initial test for test', () => {
+  test('Should return 4', () => {
+    const actual = 4;
+    const expected = 4;
+    expect(actual).toEqual(expected);
+  });
+});
