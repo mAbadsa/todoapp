01b4904f2d5e05e3bbac0f98484576f79035f1d8
Muhammad Alabadsa <moh22389@gmail.com>
2020-12-21 12:40:08 +0200

    fix test

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 exports.serverErrors = (err, req, res, next) => {
-  res.status(err.status || 500).json({
+  console.log(err);
+  return res.status(err.status || 500).json({
     success: false,
     message: err.msg,
     statusCode: err.status,
